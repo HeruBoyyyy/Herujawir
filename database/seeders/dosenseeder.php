4c7d772d4352e2082e4a247936657ca301e15d10
Herujawir <heruajaaa007@gmail.com>
2023-12-22 14:43:03 +0700

    Final Project

---
+++ database/seeders/dosenseeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class dosenseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('dosens')->insert([
+            'nidn' => "121212",
+            'nama' => 'Dosen12',
+            'alamat' => 'Padang',
+            'no_hp' => '0812121212',
+        ]);
+    }
+}
